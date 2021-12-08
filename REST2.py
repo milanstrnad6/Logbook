4fec3e30cc0d1e5a0a81d5d1af05f7d7b67b4c5e
Milan Strnad <milanstrnad@MacBook-Pro-uzivatela-Milan.local>
2021-12-08 13:56:22 +0100

    Implement put with date

--- REST2.py
+++ REST2.py
@@ -1,8 +1,13 @@
 import requests
+import TIMES
 
 url = 'https://carassistant-479b4-default-rtdb.europe-west1.firebasedatabase.app/users.json'
-#myobj = {"someKey": 'someValue'}
-x = requests.post(url, json={'someKey': 'someValue'})
-#x = requests.post(url, data = myobj)
 
+date = TIMES.now()
+dateString = TIMES.stringFrom(date)
+
+jsonData = {"dateString": 'POINTS'}
+
+#SEND REQUEST
+x = requests.put(url, json=jsonData)
 print(x.text)
