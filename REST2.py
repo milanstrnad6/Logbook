182eef325dd2778d74d3cd5554da747e7bbac5d2
Milan Strnad <milanstrnad@MacBook-Pro-uzivatela-Milan.local>
2021-12-08 13:51:09 +0100

    Hotfix

--- REST2.py
+++ REST2.py
@@ -1,8 +1,8 @@
 import requests
 
 url = 'https://carassistant-479b4-default-rtdb.europe-west1.firebasedatabase.app/users.json'
-myobj = {'someKey': 'someValue'}
-
-x = requests.post(url, data = myobj)
+#myobj = {"someKey": 'someValue'}
+x = requests.post(url, json={'someKey': 'someValue'})
+#x = requests.post(url, data = myobj)
 
 print(x.text)
