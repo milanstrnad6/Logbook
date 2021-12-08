5b5288ab4bb3ac64d5e22f97ab44804a9d1dca4a
Milan Strnad <milanstrnad@MacBook-Pro-uzivatela-Milan.local>
2021-12-08 19:11:27 +0100

    Hotfix

--- SENDING.py
+++ SENDING.py
@@ -3,16 +3,14 @@ import POINTS
 import time
 
 def start():
-	print "REST – START"
-	while True:
-		try:
-			sleeptime = 10
-			print "REST – SLEEP FOR ", sleeptime
-			time.sleep(sleeptime)
-			buffer = POINTS.load_allEvents()
-			POINTS.reset()
-			print("REST – SEND...")
-			REST.sendPoints(buffer)
-		except:
-			time.sleep(10)
-			continue
+        while True:
+                try:
+                        print "REST SLEEP FOR 10s"
+                        time.sleep(10)
+                        buffer = POINTS.load_allEvents()
+                        POINTS.reset()
+                        print "REST SEND..."
+                        REST.sendPoints(buffer)
+                except:
+                        time.sleep(10)
+                        continue
