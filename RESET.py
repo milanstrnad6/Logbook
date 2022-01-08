4f383fddcf00aad05bc4f999a4968f373e9e264c
Milan Strnad <milan.strnad@firma.seznam.cz>
2022-01-08 17:39:09 +0100

    Hotfix

--- RESET.py
+++ RESET.py
@@ -7,6 +7,6 @@ FILENAME_CURRENT_RIDE = 'currentRide.txt'
 def reset():
 	LOGGER.resetBaseFile()
 	POINTS.reset()
-    FILES.resetWithData(FILENAME_CURRENT_RIDE,"0\n1\n2\n")
+	FILES.resetWithData(FILENAME_CURRENT_RIDE,"0\n1\n2\n")
 
 reset()
