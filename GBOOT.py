e8377b8101e4a0f42192824897594405c4e0b399
Milan Strnad <milan.strnad@firma.seznam.cz>
2022-01-09 16:41:02 +0100

    Update

--- GBOOT.py
+++ GBOOT.py
@@ -3,9 +3,15 @@ import SENDING
 import time
 import DEBUG
 
+print("1")
 DEBUG.log("GBOOT.py|")
+print("2")
 duration = 30
+print("3")
 DEBUG.log("GBOOT sleep for 20s|")
+print("4")
 time.sleep(duration)
+print("5")
 DEBUG.log("GBOOT after sleep G.start()|")
+print("6")
 G.start()
